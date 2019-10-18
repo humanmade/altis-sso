90f2c782adaafe7a0040de90fe790564d0b06dcc
Tom J Nowell <contact@tomjn.com>
2019-10-18 15:31:47 +0100

    adjusted the delegated_oauth.sync-roles filter to use __return_true and __return_false depending on the value

--- inc/wordpress/namespace.php
+++ inc/wordpress/namespace.php
@@ -15,7 +15,7 @@ function load_plugin() {
 	define( 'HM_DELEGATED_AUTH_REST_BASE', $config['server-rest-base'] );
 	define( 'HM_DELEGATED_AUTH_CLIENT_ID', $config['oauth2-client-id'] );
 	define( 'HM_DELEGATED_AUTH_LOGIN_TEXT', __( 'Login with WordPress SSO', 'altis' ) );
-	add_filter( 'delegated_oauth.sync-roles', '__return_false' );
+	add_filter( 'delegated_oauth.sync-roles', ( empty( $config['sync-roles'] ) || ! $config['sync-roles'] ) ? '__return_false' : '__return_true' );
 
 	require_once ROOT_DIR . '/vendor/humanmade/delegated-oauth/plugin.php';
 }
