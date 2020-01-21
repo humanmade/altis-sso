c34809c8edf7d743f092a4e5aac8094049d145f4
Faishal Saiyed <saiyedfaishal@gmail.com>
2020-01-21 18:16:18 +0000

    Do not set HM_DELEGATED_AUTH_CLIENT_ID if client id is empty to disable cookie auth

--- inc/wordpress/namespace.php
+++ inc/wordpress/namespace.php
@@ -13,7 +13,12 @@ function load_plugin() {
 	$config = get_config()['modules']['sso']['wordpress'];
 
 	define( 'HM_DELEGATED_AUTH_REST_BASE', $config['server-rest-base'] );
-	define( 'HM_DELEGATED_AUTH_CLIENT_ID', $config['oauth2-client-id'] );
+
+	// Do not set HM_DELEGATED_AUTH_CLIENT_ID if client id is empty to disable cookie auth
+	if ( ! empty( $config['oauth2-client-id'] ) ) {
+		define( 'HM_DELEGATED_AUTH_CLIENT_ID', $config['oauth2-client-id'] );
+	}
+
 	define( 'HM_DELEGATED_AUTH_LOGIN_TEXT', __( 'Login with WordPress SSO', 'altis' ) );
 	add_filter( 'delegated_oauth.sync-roles', ( empty( $config['sync-roles'] ) || ! $config['sync-roles'] ) ? '__return_false' : '__return_true' );
 
