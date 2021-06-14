4b56ef8eeba69f55784e96046edbd67c6b990990
Ryan McCue <me@ryanmccue.info>
2021-06-14 12:47:03 +0100

    Use proper autoload

--- load.php
+++ load.php
@@ -16,8 +16,5 @@ add_action( 'altis.modules.init', function () {
 		'wordpress' => false,
 		'hide_native' => false,
 	];
-	Altis\register_module( 'sso', __DIR__, 'SSO', $default_settings, function () {
-		require __DIR__ . '/inc/namespace.php';
-		bootstrap();
-	} );
+	Altis\register_module( 'sso', __DIR__, 'SSO', $default_settings, __NAMESPACE__ . '\\bootstrap' );
 } );
